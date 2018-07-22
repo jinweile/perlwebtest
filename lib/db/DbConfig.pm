efd7b2d497c737d326fd169286b8b6cc3b8c378c
jinweile <jinweile@163.com>
2018-07-22 23:28:21 +0800

    change db to localhost

--- lib/db/DbConfig.pm
+++ lib/db/DbConfig.pm
@@ -3,6 +3,6 @@ package DbConfig;
 use lib "$Bin/../lib/db";
 
 # must set dsn user password to globle var
-our $dsn = "DBI:mysql:database=openapi;host=192.168.0.141";
+our $dsn = "DBI:mysql:database=openapi;host=localhost";
 our $user = 'root';
 our $password = '123456';
